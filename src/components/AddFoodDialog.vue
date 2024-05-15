4870f2d95bc7724b507cd315777918684724a565
Raphael <graphael.dr@gmail.com>
2024-05-15 11:55:01 -0600

    create dialogue component to add food and calories

---
+++ src/components/AddFoodDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-dialog ref="dialogRef">
+    <q-card>
+    <q-card-section>
+      <div class="text-subtitle2">Add food</div>
+    </q-card-section>
+      <q-card-section class="row no-wrap">
+        <q-input 
+        label="Food" 
+        class="col-8" 
+        v-model="entry.foodName" />
+      <q-separator vertical inset class="q-mx-sm" />
+        <q-input
+          label="Calories"
+          v-model="entry.Calories"
+        />
+    </q-card-section>
+    <q-card-section class="row">
+      <q-space/>
+      <q-btn
+        flat
+        no-caps
+        color="orange"
+        label="Cancel"
+        />
+        <q-btn
+        flat
+        no-caps
+        color="green"
+        label="Ok"
+      />
+    </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+import { defineComponent, reactive } from "vue";
+import { useDialogPluginComponent } from 'quasar'
+export default defineComponent({
+  name: 'AddFoodDialog',
+  setup(){
+    const { dialogRef } = useDialogPluginComponent()
+    const entry = reactive({
+      foodName: "",
+      Calories: 0
+    })
+    return {
+      dialogRef,
+      entry
+    }
+  }
+})
+</script>
+<style scoped lang="scss">
+  
+</style>
