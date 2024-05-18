aacf12085526a209a5952b1a821ca3df3f463df4
Raphael <graphael.dr@gmail.com>
2024-05-18 16:44:58 -0600

    add settings button drop down and date

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,47 @@
 <template>
   <q-page class="bg-grey-2">
+    <div class="row no-wrap justify-between items-center q-px-lg">
+      <div class="text-center q-py-md text-subtitle2">{{ formattedDate }}</div>
+      <div>
+        <q-btn-dropdown
+          flat
+          dropdown-icon="settings"
+          size="sm"
+          color="grey-7"
+          auto-close
+          :menu-offset="[0, 16]"
+        >
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-btn
+                  unelevated
+                  square
+                  color="blue-6"
+                  label="Target Calories"
+                  icon-right="edit"
+                  no-caps
+                  @click="editTargetCalories()"
+                />
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-btn
+                  unelevated
+                  square
+                  label="flush"
+                  color="red-6"
+                  icon-right="delete"
+                  @click="flush()"
+                />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </div>
+    </div>
+
     <!-- Top card -->
     <q-card
       flat
