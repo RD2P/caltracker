abb22da39901a113c63ad55131169fc8b35fa033
Raphael <graphael.dr@gmail.com>
2024-05-24 20:50:02 -0600

    extract top bar to its own component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="row no-wrap justify-between items-center q-px-lg">
+    <div class="text-center q-py-md text-subtitle2">
+      {{ formattedDate() }}
+    </div>
+    <div>
+      <q-btn-dropdown
+        flat
+        dropdown-icon="settings"
+        size="sm"
+        color="grey-7"
+        auto-close
+        :menu-offset="[0, 16]"
+      >
+        <q-list>
+          <q-item>
+            <q-item-section>
+              <q-btn
+                unelevated
+                square
+                color="blue-6"
+                label="Target Calories"
+                icon-right="edit"
+                no-caps
+                @click="editTargetCalories()"
+              />
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-btn
+                unelevated
+                square
+                label="flush"
+                color="red-6"
+                icon-right="delete"
+                @click="flush()"
+              />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { formattedDate } from "src/services/helpers.js";
+
+export default defineComponent({
+  name: "TopBar",
+
+  setup() {
+    return {
+      formattedDate
+    }
+  }
+});
+</script>
+
+<style lang=""></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,6 @@
 <template>
   <q-page class="bg-grey-2">
-    <div class="row no-wrap justify-between items-center q-px-lg">
-      <div class="text-center q-py-md text-subtitle2">
-        {{ formattedDate() }}
-      </div>
-      <div>
-        <q-btn-dropdown
-          flat
-          dropdown-icon="settings"
-          size="sm"
-          color="grey-7"
-          auto-close
-          :menu-offset="[0, 16]"
-        >
-          <q-list>
-            <q-item>
-              <q-item-section>
-                <q-btn
-                  unelevated
-                  square
-                  color="blue-6"
-                  label="Target Calories"
-                  icon-right="edit"
-                  no-caps
-                  @click="editTargetCalories()"
-                />
-              </q-item-section>
-            </q-item>
-            <q-item>
-              <q-item-section>
-                <q-btn
-                  unelevated
-                  square
-                  label="flush"
-                  color="red-6"
-                  icon-right="delete"
-                  @click="flush()"
-                />
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-      </div>
-    </div>
-
+    <TopBar />
     <SummaryBanner />
 
     <FoodTable />
@@ -77,10 +34,12 @@ import prefs from "src/services/preferences.js";
 import { getFireFoods, getBodyWeight } from "src/services/firestore.js";
 import SummaryBanner from "src/components/SummaryBanner.vue";
 import FoodTable from "src/components/FoodTable.vue";
+import TopBar from "src/components/TopBar.vue";
 
 export default defineComponent({
   name: "IndexPage",
   components: {
+    TopBar,
     SummaryBanner,
     FoodTable,
   },
